8b91dad97e66b9a0a112859c49f009ae85f78b7e
Stanley Huang <huang.stanley050@gmail.com>
2019-06-18 16:42:38 +1000

    trying to fix netlify issue, relates to redux

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,30 @@
  */
 
 // You can delete this file if you're not using it
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+import backgroundReducer from "./src/store/reducers/backgroundReducer";
+import React from "react";
+import rootSaga from "./src/store/sagas";
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
+const rootReducer = combineReducers({
+  background: backgroundReducer,
+});
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
+
+const wrapRootElement = ({ element }) => {
+  return <Provider store={store}>{element}</Provider>;
+};
+
+export { wrapRootElement };
